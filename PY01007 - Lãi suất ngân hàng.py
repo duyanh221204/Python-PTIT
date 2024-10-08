e803975ee4e953db77d8e2e98a13c69b6b746ebf
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-10-08 16:16:06 +0700

    Create PY01007 - Lãi suất ngân hàng.py

--- PY01007 - Lãi suất ngân hàng.py
+++ PY01007 - Lãi suất ngân hàng.py
@@ -1,8 +1,7 @@
 for T in range(int(input())):
-    n, k, m = map(float, input().split())
-    x, y = m / n, k / 100 + 1
-    d = 1
-    while x > y:
+    n, x, m = map(float, input().split())
+    d = 0
+    while n < m:
         d += 1
-        x /= y
+        n += (n * x / 100)
     print (d)
